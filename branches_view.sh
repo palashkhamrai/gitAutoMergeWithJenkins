0bda811ba04e7dd2f749700a2818950763388a7d
Palash Khamrai <46481977+palashkhamrai@users.noreply.github.com>
2024-02-18 11:50:26 +0530

    Update branches_view.sh

--- branches_view.sh
+++ branches_view.sh
@@ -1,37 +1,20 @@
 #!/bin/bash
 
 # Navigate to the Git repository
-# cd /path/to/your/git/repo
+# cd /path/to/your/git/repo || exit 1
 
 # Fetch the latest branch information
 git fetch --all || exit 1
 
-echo "start branches_view.sh"
 # Function to recursively display branches with tree-like structure
 display_branches() {
     local prefix="$1"
     local parent_branch="$2"
-    local branches=()
 
+    git for-each-ref --format='%(refname:short)' --sort=-committerdate --merged "$parent_branch" refs/heads/ | \
     while IFS= read -r branch; do
-        branches+=("$branch")
-    done < <(git for-each-ref --format='%(refname:short)' --sort=-committerdate --merged "$parent_branch" refs/heads/)
-
-    local count=${#branches[@]}
-    
-    for ((i=0; i<count; i++)); do
-        local branch="${branches[i]}"
-        local is_last=$((i == count - 1))
-
-        echo -n "$prefix"
-        echo -n "$([ $is_last ] && echo '└── ' || echo '├── ')"
-        echo "$branch"
-
-        if [ $is_last ]; then
-            display_branches "$prefix    " "$branch"
-        else
-            display_branches "$prefix│   " "$branch"
-        fi
+        echo "$prefix$branch"
+        display_branches "$prefix    " "$branch"
     done
 }
 
